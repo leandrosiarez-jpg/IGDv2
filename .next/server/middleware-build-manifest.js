33d7a601016f77e48723de6b6531e3a53b166dd0
Omega-Reached <www.marcosgutierritos.com@gmail.com>
2025-09-20 21:03:53 -0300

    fixing upload git pages

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,18 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-f4b587452b1a6380.js","static/chunks/fd9d1056-0cf176b433b91c1c.js","static/chunks/117-c4381535027dd26d.js","static/chunks/main-app-b6db834e6ac17e9b.js"],pages:{"/_app":["static/chunks/webpack-f4b587452b1a6380.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-c0bbc5e90234f794.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-f4b587452b1a6380.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-c0bbc5e90234f794.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
